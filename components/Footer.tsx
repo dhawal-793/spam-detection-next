bd0a4e8d735499cd9df6a744ab22b0be8515ffdf
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-01 23:46:30 +0530

    Created Footer component.

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+  return (
+    <footer className="py-2 text-lg text-center border-t text-light border-t-input">
+    Copyright  © {new Date().getFullYear()} | Spam Detector
+    </footer>
+  );
+};
+
+export default Footer;
