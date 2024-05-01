83a602e8cf28be1d6e1b248e3b0b1316122071b2
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-02 02:15:47 +0530

    updated nextconfig to use pwa

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withPwa = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withPwa(nextConfig);
