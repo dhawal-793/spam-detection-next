d9f00979c81882616e130fc4be84facc2de64230
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2024-05-02 11:53:45 +0530

    updated about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,18 @@
 
 const About = () => {
   return (
-    <div className='max-w-screen-md mx-auto'>
-      <h2>Spam Detection</h2>
+    <div className='max-w-screen-md mx-auto text-left'>
+      <h2 className="text-3xl md:text-5xl font-bold mb-10">Hack-A-Vishkar</h2>
+      <ul className="text-sm md:text-xl list-disc space-y-3 ">
+        <li>Spam detector is a Hack-A-Vishkar project.</li>
+        <li>Created using flask backend and nextjs frontend</li>
+        <li>We have used Random Forest Classifier for training our email model. </li>
+        <li>We have used Multinovial Naive Based for training our message model. </li>
+        <li>The flask server exposes different APIs for predicting emails and messages</li>
+        <li>The nextJs app uses those APIs to predict the result for the content.</li>
+        <li>The result then will bedisplayed as a toast notification.</li>
+      </ul>
+
     </div>
   )
 }
